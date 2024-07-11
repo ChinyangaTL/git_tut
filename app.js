3ca9fd1af059c87a7e72143ab7508ba1618f7c6c
Les Chinyanga <les.chinyanga@gmail.com>
2024-07-11 13:46:11 +0200

    add log commit information

--- app.js
+++ app.js
@@ -2,3 +2,4 @@
 // git status - view the changes to your project code
 // git add . - add all files to the staging area
 // git commit -m "Initial commit" - create a new commit with files from the staging area
+// git log - view recent commits
