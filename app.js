6e33bb3c7ee35ccf4f1885c791cee4bb077b9904
Les Chinyanga <les.chinyanga@gmail.com>
2024-07-11 13:45:23 +0200

    add basic information

---
+++ app.js
@@ -0,0 +1,4 @@
+// git init - create a new repository
+// git status - view the changes to your project code
+// git add . - add all files to the staging area
+// git commit -m "Initial commit" - create a new commit with files from the staging area
