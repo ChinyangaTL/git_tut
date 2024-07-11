abb089b06c971c6dcf3556a1be9b47e9a0fc3232
Les Chinyanga <les.chinyanga@gmail.com>
2024-07-11 14:27:36 +0200

    add remote info

--- app.js
+++ app.js
@@ -14,3 +14,4 @@
 // git branch -m <branchToChange> <newBranchName> - rename a branch
 // git merge <branchname> - merge a branch into the current branch
 // git pull origin <branchname> - pull changes from the master branch
+// git remote -v - view remote repositories
