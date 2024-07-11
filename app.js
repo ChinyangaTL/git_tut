d79dbb73395cb322896b66891a44d72ceee353b7
Les Chinyanga <les.chinyanga@gmail.com>
2024-07-11 13:47:13 +0200

    add log oneline info

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // git init - create a new repository
 // git status - view the changes to your project code
-// git add . - add all files to the staging area
+// git add <filename> - add all files to the staging area
 // git commit -m "Initial commit" - create a new commit with files from the staging area
 // git log - view recent commits
+// git log --online - view recent commits in a compact format
